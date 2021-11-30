682fdd052bc5fcb1cdc5458ecd28877034af1246
natefbennett <nate.f.bennett@gmail.com>
2021-11-29 23:59:21 -0500

    read lines from stdin

--- hw4/hw4_client.py
+++ hw4/hw4_client.py
@@ -14,12 +14,66 @@
 import sys
 import socket 
 
+def PrintCommandMenu():
+	print(
+		'Available Commands:\n' + \
+		'\tMOVE     [NewXPosition] [NewYPosition] \n' + \
+		'\tSENDDATA [DestinationID]\n' + \
+		'\tWHERE    [SensorID/BaseID]\n' + \
+		'\tQUIT\n'
+	)
+
 def run():
 
-    # check number of command line args
-    if len(sys.argv) != 4:
-        print(f'Usage: python3 -u {sys.argv[0]} [control address] [control port] [SensorID] [SensorRange] [InitalXPosition] [InitialYPosition]')
-        sys.exit(-1)
+	# check number of command line args
+	if len(sys.argv) != 4:
+		print(f'Usage: python3 -u {sys.argv[0]} [control address] [control port] [SensorID] [SensorRange] [InitialXPosition] [InitialYPosition]')
+		sys.exit(-1)
+
+	control_addr = sys.argv[1]
+	control_port = sys.argv[2]
+	sensor_id    = sys.argv[3]
+	sensor_range = sys.argv[4]
+	init_x_pos   = sys.argv[5]
+	init_y_pos   = sys.argv[6]
+
+	# read form stdin for commands
+	for line in sys.stdin:
+
+		line = line.split()
+
+		# user hit enter with no data
+		if line == []: 
+			PrintCommandMenu()
+			continue 
+
+		cmd = line.pop(0)
+
+		# command: MOVE [NewXPosition] [NewYPosition]
+		if cmd == 'MOVE':
+			# causes the sensor to update its location to the x-coordinate
+			# specified by [NewXPosition] and the y-coordinate specified by [NewYPosition].
+			# The sensor should also send an UPDATEPOSITION command to the server
+			pass
+
+		# command: SENDDATA [DestinationID]
+		elif cmd == 'SENDDATA':
+			# causes the sensor to generate a new DATAMESSAGE with a destination of
+			# [DestinationID]. The sensor should first send an UPDATEPOSITION message to the server to get an
+			# up-to-date list of reachable sensors and base stations.
+			pass
+
+		# command: WHERE [SensorID/BaseID]
+		elif cmd == 'WHERE':
+			# to get the location of a particular base station or sensor ID from the control
+			# server. It should not take any other actions until it gets a THERE message back from the server.
+			pass
+
+		# command: QUIT
+		elif cmd == 'QUIT':
+			# causes the client program to clean up any memory and any 
+			# sockets that are in use, and then terminate.
+			pass
 
 if __name__ == '__main__':
 	run()
--- hw4/hw4_control.py
+++ hw4/hw4_control.py
@@ -14,12 +14,39 @@
 import sys
 import socket 
 
+def PrintCommandMenu():
+	print(
+		'Available Commands:\n' + \
+		'\tQUIT\n'
+	)
+
 def run():
-    
-    # check number of command line args
-    if len(sys.argv) != 4:
-        print(f'Usage: python3 -u {sys.argv[0]} [control port] [base station file]')
-        sys.exit(-1)
+	
+	# check number of command line args
+	if len(sys.argv) != 4:
+		print(f'Usage: python3 -u {sys.argv[0]} [control port] [base station file]')
+		sys.exit(-1)
+
+	control_port      = sys.argv[1]
+	base_station_file = sys.argv[2]
+
+	# read form stdin for commands
+	for line in sys.stdin:
+
+		line = line.split()
+
+		# user hit enter with no data
+		if line == []: 
+			PrintCommandMenu()
+			continue 
+
+		cmd = line.pop(0)
+
+		# command: QUIT
+		if cmd == 'QUIT':
+			# causes the client program to clean up any memory and any 
+			# sockets that are in use, and then terminate.
+			pass
 
 if __name__ == '__main__':
 	run()
