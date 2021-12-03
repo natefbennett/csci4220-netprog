c4eb74de9f61f59b3ab155e099ba4cb0e3139f92
natefbennett <nate.f.bennett@gmail.com>
2021-12-03 00:58:55 -0500

    added sensor class with UpdatePosition method

--- hw4/hw4_client.py
+++ hw4/hw4_client.py
@@ -12,7 +12,68 @@
 # Usage:  python3 -u hw4_client.py [control address] [control port] [SensorID] [SensorRange] [InitalXPosition] [InitialYPosition]
 
 import sys
-import socket 
+import socket
+
+# sensor class to store the current state and send required messages
+class Sensor:
+	
+	def __init__(self, id, x, y, c_addr, c_port, range):
+		self.id     = id
+		self.x      = x
+		self.y      = y
+		self.c_addr = c_addr
+		self.c_port = c_port
+		self.c_sock = None
+		self.range  = range
+
+	def Connect(self):
+		# Create the TCP socket, connect to the server
+		self.c_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+		# bind takes a 2-tuple, not 2 arguments
+		self.c_sock.connect((self.c_addr, self.c_port))
+
+	def Disconnect(self):
+		self.c_sock.close()
+
+	# offer optional perameters, incase there are no new coords to send
+	def UpdatePosition(self, new_x=-1, new_y=-1):
+		
+		send_x = new_y
+		send_y = new_x
+
+		# if no position perameters passed, keep current position
+		if new_x != -1 and new_y != -1:
+			send_x = self.x
+			send_y = self.y
+
+		# send message to control server
+		msg = f'UPDATEPOSITION {self.id} {self.range} {send_x} {send_y}'
+		self.c_sock.sendall(msg.encode('utf-8'))
+
+		# wait for reachable message to be recieved
+		recv_msg = self.c_sock.recv(1024).decode('utf-8')
+
+		# sort the reachable list by their [SensorID]/[BaseID]
+		recv_list = recv_msg.split()
+		if recv_list.pop(0) == 'REACHABLE':
+			num_reachable = recv_list.pop(0)
+			recv_list = recv_list.sort()
+
+			print(f'{self.id}: After reading REACHABLE message, I can see: {recv_list}')
+		else:
+			# wrong message received
+			pass
+		
+	def SendDataMessage(self, dest_id):
+		# self.c_sock.sendall(send_string.encode('utf-8'))
+		# get the response from the server
+    	# recv_string = self.c_sock.recv(1024)
+		pass
+
+	def Where(self, id):
+		pass
+
 
 def PrintCommandMenu():
 	print(
@@ -30,12 +91,22 @@ def run():
 		print(f'Usage: python3 -u {sys.argv[0]} [control address] [control port] [SensorID] [SensorRange] [InitialXPosition] [InitialYPosition]')
 		sys.exit(-1)
 
-	control_addr = sys.argv[1]
-	control_port = sys.argv[2]
+	control_addr = sys.argv[1]      # address control server can be found
+	control_port = int(sys.argv[2]) # port control server can be found
 	sensor_id    = sys.argv[3]
 	sensor_range = sys.argv[4]
 	init_x_pos   = sys.argv[5]
 	init_y_pos   = sys.argv[6]
+
+	control_addr = socket.gethostbyname(control_addr)
+
+	# initialize object to manage server operations
+	sensor = Sensor(sensor_id, init_x_pos, init_y_pos, control_addr, control_port, sensor_range)
+
+	# connect to control server
+	sensor.Connect()
+
+	# TODO: send an UPDATEPOSITION message to control server
 
 	# read form stdin for commands
 	for line in sys.stdin:
